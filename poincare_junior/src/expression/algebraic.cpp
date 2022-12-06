fc43c58ee36742409ef11d90ba7abbbe46cb9891
Emilie Feral <emilie.feral@numworks.com>
2022-12-06 13:34:59 +0100

    [poincare_junior] Draft of Algebraic class

---
+++ poincare_junior/src/expression/algebraic.cpp
@@ -0,0 +1,94 @@
+#include "algebraic.h"
+#include <poincare_junior/src/n_ary.h>
+#include <poincare_junior/src/memory/node_iterator.h>
+#include <poincare_junior/src/memory/tree_constructor.h>
+#include "rational.h"
+#include "simplification.h"
+
+namespace Poincare {
+
+EditionReference Algebraic::Rationalize(EditionReference expression) {
+  BlockType type = expression.type();
+  if (type == BlockType::Power) {
+    Rationalize(expression.childAtIndex(0));
+    return expression; // TODO return basicReduction
+  }
+  if (type == BlockType::Multiplication) {
+    for (std::pair<EditionReference, int> indexedNode : NodeIterator::Children<Forward, Editable>(expression)) {
+      Rationalize(std::get<EditionReference>(indexedNode));
+    }
+    return expression; // TODO return basicReduction
+  }
+  if (type == BlockType::Addition) {
+    // Factorize on common denominator a/b + c/d
+    return RationalizeAddition(expression);
+  }
+  return expression;
+}
+
+EditionReference Algebraic::RationalizeAddition(EditionReference expression) {
+  assert(expression.type() == BlockType::Addition);
+  EditionReference commonDenominator = EditionReference(Mult());
+  // Step 1: We want to compute the common denominator, b*d
+  for (std::pair<EditionReference, int> indexedNode : NodeIterator::Children<Forward, Editable>(expression)) {
+    EditionReference child = std::get<EditionReference>(indexedNode);
+    child = Rationalize(child);
+    EditionReference denominator = Denominator(EditionReference::Clone(child.node()));
+    NAry::AddChild(commonDenominator, denominator); // FIXME: do we need LCM?
+  }
+  // basic reduction commonDenominator
+  assert(commonDenominator.type() != BlockType::Zero);
+  if (commonDenominator.type() == BlockType::One) {
+    return expression;
+  }
+  /* Step 2: Turn the expression into the numerator. We start with this being a/b+c/d and we
+   * want to create numerator = a/b*b*d + c/d*b*d = a*d + c*b */
+  for (std::pair<EditionReference, int> indexedNode : NodeIterator::Children<Forward, Editable>(expression)) {
+    EditionReference child = std::get<EditionReference>(indexedNode);
+    // Create Mult(child, commonDenominator) = a*b * b*d
+    EditionReference multiplication = EditionReference::Push<BlockType::Multiplication>(1);
+    child.insertNodeBeforeNode(multiplication);
+    child.nextTree().insertTreeBeforeNode(EditionReference::Clone(commonDenominator.node()));
+    // TODO basicReduction of child
+  }
+  // Create Pow(commonDenominator, -1)
+  EditionReference power = EditionReference::Push<BlockType::Power>();
+  commonDenominator.insertNodeBeforeNode(power);
+  commonDenominator.nextTree().insertTreeBeforeNode(1_nsn);
+  // TODO basicReduction of power
+  // Create Mult(expression, Pow)
+  EditionReference fraction = EditionReference::Push<BlockType::Multiplication>(2);
+  expression.insertNodeBeforeNode(fraction);
+  expression.nextTree().insertTreeBeforeNode(commonDenominator);
+  // TODO basicReduction of fraction
+  return fraction;
+}
+
+EditionReference Algebraic::NormalFormator(EditionReference expression, bool numerator) {
+  if (expression.block()->isRational()) {
+    IntegerHandler ator = numerator ? Rational::Numerator(expression.node()) : Rational::Denominator(expression.node());
+    expression.replaceNodeByNode(Integer::PushNode(ator));
+    return expression;
+  }
+  BlockType type = expression.type();
+  if (type == BlockType::Power) {
+    EditionReference exponent = expression.childAtIndex(1);
+    bool negativeRationalExponent = exponent.block()->isRational() && Rational::Sign(exponent.node()) < 0;
+    if (negativeRationalExponent) {
+      Rational::SetSign(exponent, false);
+    }
+    EditionReference result = numerator == negativeRationalExponent ? EditionReference(1_sn) : expression; // TODO: BasicReduction(expression)
+    expression.replaceTreeByTree(result);
+    return result;
+  }
+  if (type == BlockType::Multiplication) {
+    for (std::pair<EditionReference, int> indexedNode : NodeIterator::Children<Forward, Editable>(expression)) {
+      EditionReference child = std::get<EditionReference>(indexedNode);
+      child = NormalFormator(child, numerator);
+    }
+    // TODO basicReduction of expression
+  }
+  return expression;
+}
+
+}
---
+++ poincare_junior/src/expression/algebraic.h
@@ -0,0 +1,29 @@
+#ifndef POINCARE_EXPRESSION_ALGEBRAIC_H
+#define POINCARE_EXPRESSION_ALGEBRAIC_H
+
+#include <poincare_junior/src/memory/edition_reference.h>
+
+namespace Poincare {
+
+class Algebraic final {
+public:
+  static void Expand(EditionReference node);
+  // develop product of sum
+  // develop integer power of sum
+  // -->be recursive on the results above
+  // develop the integer part of power of non-integer?
+  // what about subexpressions? cos(x*(x+1))? NO?
+  static EditionReference Rationalize(EditionReference node);
+  static EditionReference SideRelations(Node expression);
+
+  static EditionReference Numerator(EditionReference expression) { return NormalFormator(expression, true); }
+  static EditionReference Denominator(EditionReference expression) { return NormalFormator(expression, false); }
+
+private:
+  static EditionReference RationalizeAddition(EditionReference expression);
+  static EditionReference NormalFormator(EditionReference expression, bool numerator);
+};
+
+}
+
+#endif
