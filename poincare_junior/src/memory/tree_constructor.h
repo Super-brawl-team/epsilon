dc620474dfb05ee4463989d2e586780417f5b39e
Emilie Feral <emilie.feral@numworks.com>
2022-12-26 14:53:02 +0100

    [poincare_junior] Add a constexpr Polynomial constructor

--- poincare_junior/src/memory/tree_constructor.h
+++ poincare_junior/src/memory/tree_constructor.h
@@ -21,6 +21,30 @@ private:
   TypeBlock m_blocks[N];
 };
 
+template <BlockType blockType, unsigned N, typename... Types>
+constexpr static void CreateNode(Tree<N> * tree, Types... args) {
+  size_t i = 0;
+  while (!NodeConstructor::CreateBlockAtIndexForType<blockType>(&(*tree)[i], i, args...)) {
+    i++;
+  }
+}
+
+template<unsigned N, unsigned ...Len>
+constexpr static auto MakeChildren(Tree<N> * tree, size_t blockIndex, const Tree<Len> (&...nodes)) {
+  size_t childIndex = 0;
+  int childrenSizes[] = {Len...};
+  std::initializer_list<Node> childrenNodes{static_cast<Node>(nodes)...};
+  for (Node node : childrenNodes) {
+    // We can't use node.copyTreeTo(tree.blockAtIndex(blockIndex++)) because memcpy isn't constexpr
+    // TODO: use constexpr version of memcpy in copyTreeTo?
+    for (size_t i = 0; i < childrenSizes[childIndex]; i++) {
+      (*tree)[blockIndex++] = *(node.block() + i);
+    }
+    childIndex++;
+  }
+  return tree;
+}
+
 template<BlockType type, unsigned ...Len>
 constexpr static auto MakeTree(const Tree<Len> (&...nodes)) {
   // Compute the total length of the children
@@ -31,18 +55,7 @@ constexpr static auto MakeTree(const Tree<Len> (&...nodes)) {
   Tree<k_numberOfChildrenBlocks + numberOfBlocksInNode> tree;
   CreateNode<type>(&tree, k_numberOfChildren);
 
-  size_t blockIndex = numberOfBlocksInNode;
-  size_t childIndex = 0;
-  int childrenSizes[] = {Len...};
-  std::initializer_list<Node> childrenNodes{static_cast<Node>(nodes)...};
-  for (Node node : childrenNodes) {
-    // We can't use node.copyTreeTo(tree.blockAtIndex(blockIndex++)) because memcpy isn't constexpr
-    // TODO: use constexpr version of memcpy in copyTreeTo?
-    for (size_t i = 0; i < childrenSizes[childIndex]; i++) {
-      tree[blockIndex++] = *(node.block() + i);
-    }
-    childIndex++;
-  }
+  MakeChildren(&tree, numberOfBlocksInNode, nodes...);
   return tree;
 }
 
@@ -58,12 +71,33 @@ template<unsigned L1, unsigned L2> static constexpr Tree<L1+L2+1> Pow(const Tree
 
 template<unsigned L1, unsigned L2> static constexpr Tree<L1+L2+1> Sub(const Tree<L1> child1, const Tree<L2> child2) { return MakeTree<BlockType::Subtraction>(child1, child2); }
 
-template <BlockType blockType, unsigned N, typename... Types>
-constexpr static void CreateNode(Tree<N> * tree, Types... args) {
-  size_t i = 0;
-  while (!NodeConstructor::CreateBlockAtIndexForType<blockType>(&(*tree)[i], i, args...)) {
-    i++;
+static constexpr auto Pol() {
+  Tree<TypeBlock::NumberOfMetaBlocks(BlockType::Polynomial)> tree;
+  tree[0] = TypeBlock(BlockType::Polynomial);
+  tree[1] = 0;
+  tree[2] = 0;
+  tree[3] = TypeBlock(BlockType::Polynomial);
+  return tree;
+}
+
+template<unsigned ...Len> static constexpr auto Pol(uint8_t exponents[sizeof...(Len)], const Tree<Len> (&...coefficients)) {
+  // Compute the total length of the children
+  constexpr unsigned k_numberOfChildren = sizeof...(Len);
+  constexpr unsigned k_numberOfChildrenBlocks = (0 + ... + Len);
+  constexpr size_t numberOfBlocksInNode = TypeBlock::NumberOfMetaBlocks(BlockType::Polynomial) + k_numberOfChildren;
+
+  Tree<k_numberOfChildrenBlocks + numberOfBlocksInNode> tree;
+  size_t currentTreeIndex = 0;
+  tree[currentTreeIndex++] = TypeBlock(BlockType::Polynomial);
+  tree[currentTreeIndex++] = k_numberOfChildren;
+  for (size_t i = 0; i < k_numberOfChildren; i++) {
+    tree[currentTreeIndex++] = exponents[i];
   }
+  tree[currentTreeIndex++] = k_numberOfChildren;
+  tree[currentTreeIndex++] = TypeBlock(BlockType::Polynomial);
+
+  MakeChildren(&tree, currentTreeIndex, coefficients...);
+  return tree;
 }
 
 constexpr Tree<TypeBlock::NumberOfMetaBlocks(BlockType::Constant)> operator "" _n(char16_t name) {
