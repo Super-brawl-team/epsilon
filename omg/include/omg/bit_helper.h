db0b44e4380486db63b2c1c4db554f5d5a5c2580
Emilie Feral <emilie.feral@numworks.com>
2023-02-16 16:00:06 +0100

    [omg] BitHelper: make numberOfBitsToCountUpTo works with small values

--- omg/include/omg/bit_helper.h
+++ omg/include/omg/bit_helper.h
@@ -87,8 +87,7 @@ constexpr inline size_t indexOfMostSignificantBit(uint32_t i) {
 }
 
 constexpr inline size_t numberOfBitsToCountUpTo(uint32_t i) {
-  assert(i >= 2);
-  return indexOfMostSignificantBit(i - 1) + 1;
+  return i == 0 ? 0 : indexOfMostSignificantBit(i - 1) + 1;
 }
 
 template <typename T>
