9feb00b8e136f7d12752414e52d214a0996ca018
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2023-06-22 14:43:59 +0200

    [pcj/tets] Activate some tests

--- poincare_junior/test/derivation.cpp
+++ poincare_junior/test/derivation.cpp
@@ -33,17 +33,18 @@ QUIZ_CASE(pcj_derivation) {
   assert_derivation_inplace_is(KDiff("x"_e, "x"_e, 2_e), 1_e);
   assert_derivation_inplace_is(KDiff(23_e, "x"_e, 1_e), 0_e);
 
+  assert_derivation_is(KAdd(1_e, "x"_e), 1_e);
+  assert_derivation_is(KTrig(KLn("x"_e), 1_e),
+                       KMult(KCos(KLn("y"_e)), KPow("y"_e, -1_e)));
+
   // TODO: Activate these tests once systemReduce is fully operational
 #if 0
-  assert_derivation_is(KAdd(1_e, "x"_e), 1_e);
   assert_derivation_is(
       KMult(KPow("x"_e, 4_e), KLn("x"_e), KExp(KMult(3_e, "x"_e))),
       KAdd(KMult(4_e, KPow("y"_e, 3_e), KExp(KMult(3_e, "y"_e))),
            KMult(KPow("y"_e, 3_e), KExp(KMult(3_e, "y"_e))),
            KMult(3_e, KExp(KMult(3_e, "y"_e)), KPow("y"_e, 4_e), KLn("y"_e))));
-  assert_derivation_is(KTrig(KLn("x"_e), 1_e),
-                     KMult(KPow("y"_e, -1_e), KTrig(KLn("y"_e), 0_e)));
   assert_derivation_is(KPow(KDiff(KPow("x"_e, 2_e), "x"_e, "x"_e), 2_e),
-                     KMult(8_e, "y"_e));
+                       KMult(8_e, "y"_e));
 #endif
 }
