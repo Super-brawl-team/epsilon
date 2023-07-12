36515a5cfd63d3b94961b569ba14cabb32f91dc7
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2023-07-12 15:05:11 +0200

    [pcj/test] Add tests on rationals

---
+++ poincare_junior/test/rational.cpp
@@ -0,0 +1,133 @@
+#include <poincare_junior/src/expression/k_tree.h>
+#include <poincare_junior/src/expression/rational.h>
+#include <poincare_junior/src/expression/simplification.h>
+#include <quiz.h>
+
+#include "helper.h"
+
+using namespace PoincareJ;
+
+static inline bool integer_handler_same_absolute_value(IntegerHandler a,
+                                                       IntegerHandler b) {
+  a.setSign(NonStrictSign::Positive);
+  b.setSign(NonStrictSign::Positive);
+  return IntegerHandler::Compare(a, b) == 0;
+}
+
+static inline void assert_properties(const Tree* numerator,
+                                     const Tree* denominator,
+                                     BlockType expectedBlockType,
+                                     StrictSign strictSign) {
+  Tree* r = Rational::Push(numerator, denominator);
+  quiz_assert(r->type() == expectedBlockType);
+  quiz_assert(strictSign == Rational::StrictSign(r));
+  integer_handler_same_absolute_value(Integer::Handler(numerator),
+                                      Rational::Numerator(r));
+  integer_handler_same_absolute_value(Integer::Handler(denominator),
+                                      Rational::Denominator(r));
+  r->removeTree();
+}
+
+QUIZ_CASE(pcj_rational_properties) {
+  assert_properties(3_e, 8_e, BlockType::RationalShort, StrictSign::Positive);
+  assert_properties(-1_e, 255_e, BlockType::RationalShort,
+                    StrictSign::Negative);
+  // TODO : Handle negative denominator in Rational::Push
+  // assert_properties(1_e, -1_e, BlockType::RationalShort,
+  // StrictSign::Negative);
+  assert_properties(0_e, 5_e, BlockType::RationalShort, StrictSign::Null);
+  // TODO : Fix RationalPosBig
+  // assert_properties(32134123_e, 812312312_e, BlockType::RationalPosBig,
+  //                   StrictSign::Positive);
+  // assert_properties(32134123_e, -812312312_e, BlockType::RationalNegBig,
+  //                   StrictSign::Negative);
+  // assert_properties(-32134123_e, 812312312_e, BlockType::RationalNegBig,
+  //                   StrictSign::Negative);
+  // assert_properties(-32134123_e, -812312312_e, BlockType::RationalPosBig,
+  //                   StrictSign::Positive);
+  // assert_properties(0_e, 812312312_e, BlockType::RationalPosBig,
+  //                   StrictSign::Null);
+  // assert_properties(0_e, -812312312_e, BlockType::RationalPosBig,
+  //                   StrictSign::Null);
+}
+
+static inline void assert_irreducible_form(const Tree* iNumerator,
+                                           const Tree* iDenominator,
+                                           const Tree* resNumerator,
+                                           const Tree* resDenominator) {
+  Tree* i = Rational::Push(iNumerator, iDenominator);
+  Tree* expected = Rational::Push(resNumerator, resDenominator);
+  Tree* result = Rational::IrreducibleForm(i);
+  quiz_assert(result->treeIsIdenticalTo(expected));
+  result->removeTree();
+  expected->removeTree();
+  i->removeTree();
+}
+
+QUIZ_CASE(pcj_rational_irreducible_form) {
+  assert_irreducible_form(2_e, 6_e, 1_e, 3_e);
+  // TODO : Fix RationalPosBig
+  // assert_irreducible_form(-15170_e, 50061_e, -10_e, 33_e);
+  // assert_irreducible_form(123169_e, 123191_e, 123169_e, 123191_e);
+}
+
+typedef Tree* (*Operation)(const Tree* i, const Tree* j);
+
+static inline void assert_operation(const Tree* i, const Tree* j,
+                                    Operation operation, const Tree* expected) {
+  Tree* result = operation(i, j);
+  result->moveTreeOverTree(Rational::IrreducibleForm(result));
+  Simplification::ShallowSystematicReduce(result);
+  quiz_assert(result->treeIsIdenticalTo(expected));
+  result->removeTree();
+}
+
+static inline void assert_add_or_mult(
+    const Tree* iNumerator, const Tree* iDenominator, const Tree* jNumerator,
+    const Tree* jDenominator, Operation operation, const Tree* resNumerator,
+    const Tree* resDenominator) {
+  assert(operation == Rational::Addition ||
+         operation == Rational::Multiplication);
+  Tree* i = Rational::Push(iNumerator, iDenominator);
+  Tree* j = Rational::Push(jNumerator, jDenominator);
+  Tree* expected = Rational::Push(resNumerator, resDenominator);
+  expected->moveTreeOverTree(Rational::IrreducibleForm(expected));
+  assert_operation(i, j, operation, expected);
+  expected->removeTree();
+  j->removeTree();
+  i->removeTree();
+}
+
+QUIZ_CASE(pcj_rational_addition) {
+  assert_add_or_mult(1_e, 2_e, 1_e, 2_e, Rational::Addition, 1_e, 1_e);
+  // TODO : Fix RationalPosBig
+  // assert_add_or_mult(1237_e, 5257_e, -3_e, 4_e, Rational::Addition, -10823_e,
+  //                    21028_e);
+}
+
+QUIZ_CASE(pcj_rational_multiplication) {
+  assert_add_or_mult(1_e, 2_e, 1_e, 2_e, Rational::Multiplication, 1_e, 4_e);
+  // TODO : Fix RationalPosBig
+  // assert_add_or_mult(23515_e, 7_e, 2_e, 23515_e, Rational::Multiplication,
+  // 2_e,
+  //                    7_e);
+}
+
+static inline void assert_power(const Tree* iNumerator,
+                                const Tree* iDenominator, const Tree* j,
+                                const Tree* resNumerator,
+                                const Tree* resDenominator) {
+  Tree* i = Rational::Push(iNumerator, iDenominator);
+  Tree* expected = Rational::Push(resNumerator, resDenominator);
+  expected->moveTreeOverTree(Rational::IrreducibleForm(expected));
+  assert_operation(i, j, Rational::IntegerPower, expected);
+  expected->removeTree();
+  i->removeTree();
+}
+
+QUIZ_CASE(pcj_rational_integer_power) {
+  assert_power(3_e, 2_e, 3_e, 27_e, 8_e);
+  // TODO : Fix RationalPosBig
+  // assert_power(1_e, 2_e, 10_e, 1_e, 1024_e);
+  // assert_power(7123_e, 3_e, 2_e, 50737129_e, 9_e);
+}
