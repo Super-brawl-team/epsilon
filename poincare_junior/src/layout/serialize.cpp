86cc210d924440dc0c92592c0411f2eaf620de35
MarcNumworks <marc.coudriau@numworks.com>
2024-03-19 16:18:15 +0100

    [pcj] Produce system codepoints at serialization
    They will be removed when we connect the new parser everywhere.

--- poincare_junior/src/layout/serialize.cpp
+++ poincare_junior/src/layout/serialize.cpp
@@ -51,17 +51,17 @@ char *Serialize(const Layout *layout, char *buffer, char *end) {
       break;
     }
     case LayoutType::Fraction: {
-      buffer = append("(", buffer, end);
+      buffer = append("\x12\x12", buffer, end);
       buffer = Serialize(layout->child(0), buffer, end);
-      buffer = append(")/(", buffer, end);
+      buffer = append("\x13/\x12", buffer, end);
       buffer = Serialize(layout->child(1), buffer, end);
-      buffer = append(")", buffer, end);
+      buffer = append("\x13\x13", buffer, end);
       break;
     }
     case LayoutType::VerticalOffset: {
-      buffer = append("^(", buffer, end);
+      buffer = append("^\x12", buffer, end);
       buffer = Serialize(layout->child(0), buffer, end);
-      buffer = append(")", buffer, end);
+      buffer = append("\x13", buffer, end);
       break;
     }
     case LayoutType::OperatorSeparator:
