0a1bb3f29afab8243144a2753a6d04bd0c867843
MarcNumworks <marc.coudriau@numworks.com>
2024-03-25 14:55:02 +0100

    [poincare] Remove FindXNTSymbol1D and FindXNTSymbol2D from xnt_helpers

--- poincare/include/poincare/xnt_helpers.h
+++ poincare/include/poincare/xnt_helpers.h
@@ -36,37 +36,6 @@ CodePoint CodePointAtIndexInDefaultCycle(int index, CodePoint startingCodePoint,
 CodePoint CodePointAtIndexInCycle(int index, const CodePoint* cycle,
                                   size_t* cycleSize);
 
-// Parametered functions
-constexpr struct {
-  AliasesList aliasesList;
-  LayoutNode::Type layoutType;
-  const CodePoint* XNTcycle;
-} k_parameteredFunctions[] = {
-    {Derivative::s_functionHelper.aliasesList(),
-     LayoutNode::Type::FirstOrderDerivativeLayout, k_defaultContinuousXNTCycle},
-    {Derivative::s_functionHelper.aliasesList(),
-     LayoutNode::Type::HigherOrderDerivativeLayout,
-     k_defaultContinuousXNTCycle},
-    {Integral::s_functionHelper.aliasesList(), LayoutNode::Type::IntegralLayout,
-     k_defaultContinuousXNTCycle},
-    {Sum::s_functionHelper.aliasesList(), LayoutNode::Type::SumLayout,
-     k_defaultDiscreteXNTCycle},
-    {Product::s_functionHelper.aliasesList(), LayoutNode::Type::ProductLayout,
-     k_defaultDiscreteXNTCycle},
-    {ListSequence::s_functionHelper.aliasesList(),
-     LayoutNode::Type::ListSequenceLayout, k_defaultDiscreteXNTCycle},
-};
-constexpr int k_numberOfFunctions = std::size(k_parameteredFunctions);
-constexpr int k_indexOfMainExpression =
-    ParameteredExpression::ParameteredChildIndex();
-constexpr int k_indexOfParameter = ParameteredExpression::ParameterChildIndex();
-
-bool FindXNTSymbol1D(UnicodeDecoder& decoder, char* buffer, size_t bufferSize,
-                     int xntIndex, size_t* cycleSize);
-
-bool FindXNTSymbol2D(Layout layout, char* buffer, size_t bufferSize,
-                     int xntIndex, size_t* cycleSize);
-
 }  // namespace XNTHelpers
 
 }  // namespace Poincare
