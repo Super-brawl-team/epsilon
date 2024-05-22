e8ab2ae6b992fc7f1050d4adebe277d8ae15931a
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2024-05-22 11:34:02 +0200

    [apps/shared] Re-activate ContinuousFunctionProperties update

--- apps/shared/continuous_function_properties.cpp
+++ apps/shared/continuous_function_properties.cpp
@@ -130,8 +130,6 @@ void ContinuousFunctionProperties::update(
     SymbolType precomputedFunctionSymbol, bool isCartesianEquation) {
   reset();
   m_isInitialized = true;
-  // TODO_PCJ
-  return;
 
   setSymbolType(precomputedFunctionSymbol);
   setEquationType(precomputedOperatorType);
