8338f352068c220802b6afba30c3a1dcba81fa55
Pia Chancerel <pia.chancerel@numworks.com>
2024-07-30 14:04:15 +0200

    test(conic): add tests

--- poincare/test/old/conics.cpp
+++ poincare/test/old/conics.cpp
@@ -285,6 +285,8 @@ QUIZ_CASE(poincare_conics_parametric_shape) {
    Shape::Parabola); */
 
   quiz_assert_parametric_shape("(cos(t),sin(t))", Shape::Circle);
+  quiz_assert_parametric_shape("(cos(t),-sin(t))", Shape::Circle);
+  quiz_assert_parametric_shape("(sin(t),cos(t))", Shape::Circle);
   quiz_assert_parametric_shape("(2cos(-t+3)+π,2sin(-t+3)+0.1)", Shape::Circle);
   quiz_assert_parametric_shape("(2sin(-t+3)+π,2cos(-t+3)+0.1)", Shape::Circle);
   quiz_assert_parametric_shape("(2cos(-t+3)+π,2sin(-t+2)+0.1)", Shape::Ellipse);
