9e58c9b36b4971ae2dd4c1e348b0e6f30d51b221
MarcNumworks <marc.coudriau@numworks.com>
2024-09-06 11:42:47 +0200

    test(poincare): replace Builders by KTrees in layout_to_expression

--- poincare/test/old/layout_to_expression.cpp
+++ poincare/test/old/layout_to_expression.cpp
@@ -17,6 +17,7 @@
 #include <poincare/old/poincare_expressions.h>
 #include <poincare/old/power.h>
 #include <poincare/src/expression/conversion.h>
+#include <poincare/src/expression/k_tree.h>
 #include <poincare/src/layout/k_tree.h>
 #include <poincare/src/layout/parser.h>
 
@@ -52,167 +53,123 @@ QUIZ_CASE(poincare_layout_to_expression_unparsable) {
   }
 }
 
-void assert_parsed_layout_is(Layout l, Poincare::OExpression r) {
+void assert_parsed_layout_is(Layout l, const Poincare::Internal::Tree* rj) {
   constexpr int bufferSize = 500;
   char buffer[bufferSize];
   l.serialize(buffer, bufferSize);
 
   Internal::Tree* ej = Internal::Parser::Parse(l.tree()->cloneTree(), nullptr);
   quiz_assert_print_if_failure(ej, buffer);
-  Internal::Tree* rj = Internal::FromPoincareExpression(r);
-  quiz_assert_print_if_failure(rj, buffer);
   quiz_assert_print_if_failure(ej->treeIsIdenticalTo(rj), buffer);
   Internal::TreeStack::SharedTreeStack->flush();
 }
 
-OMatrix BuildOneChildMatrix(OExpression entry) {
-  OMatrix m = OMatrix::Builder();
-  m.addChildAtIndexInPlace(entry, 0, 0);
-  return m;
-}
-
 QUIZ_CASE(poincare_layout_to_expression_parsable) {
   Layout l;
-  OExpression e;
+  const Poincare::Internal::Tree* e;
 
   // 1+2
   l = "1+2"_l;
-  e = Addition::Builder(BasedInteger::Builder(1), BasedInteger::Builder(2));
+  e = KAdd(1_e, 2_e);
   assert_parsed_layout_is(l, e);
 
   // |3+3/6|
   l = KRackL(KAbsL("3+"_l ^ KFracL("3"_l, "6"_l)));
-  e = AbsoluteValue::Builder(Addition::Builder(
-      BasedInteger::Builder(3),
-      Division::Builder(BasedInteger::Builder(3), BasedInteger::Builder(6))));
+  e = KAbs(KAdd(3_e, KDiv(3_e, 6_e)));
   assert_parsed_layout_is(l, e);
 
   // binCoef(4,5)
   l = KRackL(KBinomialL("4"_l, "5"_l));
-  e = BinomialCoefficient::Builder(BasedInteger::Builder(4),
-                                   BasedInteger::Builder(5));
+  e = KBinomial(4_e, 5_e);
   assert_parsed_layout_is(l, e);
 
   // ceil(4.6)
   l = KRackL(KCeilL("4.6"_l));
-  e = Ceiling::Builder(Decimal::Builder(4.6));
+  e = KCeil(4.6_e);
   assert_parsed_layout_is(l, e);
 
   // floor(7.2)
   l = KRackL(KFloorL("7.2"_l));
-  e = Floor::Builder(Decimal::Builder(7.2));
+  e = KFloor(7.2_e);
   assert_parsed_layout_is(l, e);
 
   // 2^(3+4)
   l = "2"_l ^ KSuperscriptL("3+4"_l);
-  e = Power::Builder(
-      BasedInteger::Builder(2),
-      Addition::Builder(BasedInteger::Builder(3), BasedInteger::Builder(4)));
+  e = KPow(2_e, KAdd(3_e, 4_e));
   assert_parsed_layout_is(l, e);
 
   // 2^(3+4)!
   l = "2"_l ^ KSuperscriptL("3+4"_l) ^ "!"_l;
-  e = Factorial::Builder(Power::Builder(
-      BasedInteger::Builder(2),
-      Addition::Builder(BasedInteger::Builder(3), BasedInteger::Builder(4))));
+  e = KFact(KPow(2_e, KAdd(3_e, 4_e)));
   assert_parsed_layout_is(l, e);
 
   // 2^3^4
   l = "2"_l ^ KSuperscriptL("3"_l) ^ KSuperscriptL("4"_l);
-  e = Power::Builder(
-      BasedInteger::Builder(2),
-      Power::Builder(BasedInteger::Builder(3), BasedInteger::Builder(4)));
+  e = KPow(2_e, KPow(3_e, 4_e));
   assert_parsed_layout_is(l, e);
 
   // log_3(2)
   l = "log"_l ^ KSubscriptL("3"_l) ^ KParenthesesL("2"_l);
-  e = Logarithm::Builder(BasedInteger::Builder(2), BasedInteger::Builder(3));
+  e = KLogBase(2_e, 3_e);
   assert_parsed_layout_is(l, e);
 
   /* 3
    *  log(2) */
   l = KPrefixSuperscriptL("3"_l) ^ "log"_l ^ KParenthesesL("2"_l);
-  e = Logarithm::Builder(BasedInteger::Builder(2), BasedInteger::Builder(3));
+  e = KLogBase(2_e, 3_e);
   assert_parsed_layout_is(l, e);
 
   // root(5,3)
   l = KRackL(KRootL("5"_l, "3"_l));
-  e = NthRoot::Builder(BasedInteger::Builder(5), BasedInteger::Builder(3));
+  e = KRoot(5_e, 3_e);
   assert_parsed_layout_is(l, e);
 
   // int(7, x, 4, 5)
   l = KRackL(KIntegralL("x"_l, "4"_l, "5"_l, "7"_l));
-  e = Integral::Builder(BasedInteger::Builder(7), Symbol::Builder('x'),
-                        BasedInteger::Builder(4), BasedInteger::Builder(5));
+  e = KIntegral("x"_e, 4_e, 5_e, 7_e);
   assert_parsed_layout_is(l, e);
 
   // 2^2 !
   l = "2"_l ^ KSuperscriptL("2"_l) ^ "!"_l;
-  e = Factorial::Builder(
-      Power::Builder(BasedInteger::Builder(2), BasedInteger::Builder(2)));
+  e = KFact(KPow(2_e, 2_e));
   assert_parsed_layout_is(l, e);
 
   // 5* 6/(7+5) *3
   l = "5"_l ^ KFracL("6"_l, "7+5"_l) ^ "3"_l;
-  e = Multiplication::Builder(
-      BasedInteger::Builder(5),
-      Division::Builder(BasedInteger::Builder(6),
-                        Addition::Builder(BasedInteger::Builder(7),
-                                          BasedInteger::Builder(5))),
-      BasedInteger::Builder(3));
+  e = KMult(5_e, KDiv(6_e, KAdd(7_e, 5_e)), 3_e);
   assert_parsed_layout_is(l, e);
 
   // [[3^2!, 7][4,5]
   l = KRackL(
       KMatrix2x2L("3"_l ^ KSuperscriptL("2"_l) ^ "!"_l, "7"_l, "4"_l, "5"_l));
-  OMatrix m = BuildOneChildMatrix(Factorial::Builder(
-      Power::Builder(BasedInteger::Builder(3), BasedInteger::Builder(2))));
-  m.addChildAtIndexInPlace(BasedInteger::Builder(7), 1, 1);
-  m.addChildAtIndexInPlace(BasedInteger::Builder(4), 2, 2);
-  m.addChildAtIndexInPlace(BasedInteger::Builder(5), 3, 3);
-  m.setDimensions(2, 2);
-  e = m;
+  e = KMatrix<2, 2>(KFact(KPow(3_e, 2_e)), 7_e, 4_e, 5_e);
   assert_parsed_layout_is(l, e);
 
   // 2^det([[3!, 7][4,5])
   l = "2"_l ^ KSuperscriptL("det"_l ^ KParenthesesL(KRackL(KMatrix2x2L(
                                           "3!"_l, "7"_l, "4"_l, "5"_l))));
-  m = BuildOneChildMatrix(Factorial::Builder(BasedInteger::Builder(3)));
-  m.addChildAtIndexInPlace(BasedInteger::Builder(7), 1, 1);
-  m.addChildAtIndexInPlace(BasedInteger::Builder(4), 2, 2);
-  m.addChildAtIndexInPlace(BasedInteger::Builder(5), 3, 3);
-  m.setDimensions(2, 2);
-  e = Power::Builder(BasedInteger::Builder(2), Determinant::Builder(m));
+  e = KPow(2_e, KDet(KMatrix<2, 2>(KFact(3_e), 7_e, 4_e, 5_e)));
   assert_parsed_layout_is(l, e);
 
   // 2e^3
   l = "2e"_l ^ KSuperscriptL("3"_l);
-  e = Multiplication::Builder(BasedInteger::Builder(2),
-                              Power::Builder(Constant::ExponentialEBuilder(),
-                                             BasedInteger::Builder(3)));
+  e = KMult(2_e, KPow(e_e, 3_e));
   assert_parsed_layout_is(l, e);
 
   // x|2|
   l = "x"_l ^ KAbsL("2"_l);
-  e = Multiplication::Builder(Symbol::Builder('x'),
-                              AbsoluteValue::Builder(BasedInteger::Builder(2)));
+  e = KMult("x"_e, KAbs(2_e));
   assert_parsed_layout_is(l, e);
 
   // x int(7, x, 4, 5)
   l = "x"_l ^ KIntegralL("x"_l, "4"_l, "5"_l, "7"_l);
-  e = Multiplication::Builder(
-      Symbol::Builder('x'),
-      Integral::Builder(BasedInteger::Builder(7), Symbol::Builder('x'),
-                        BasedInteger::Builder(4), BasedInteger::Builder(5)));
+  e = KMult("x"_e, KIntegral("x"_e, 4_e, 5_e, 7_e));
   assert_parsed_layout_is(l, e);
 
   // diff(1/Var, Var, cos(2))
   l = KRackL(
       KDiffL("Var"_l, "cos(2)"_l, "1"_l, KRackL(KFracL("1"_l, "Var"_l))));
-  e = Derivative::Builder(
-      Division::Builder(BasedInteger::Builder(1), Symbol::Builder("Var", 3)),
-      Symbol::Builder("Var", 3), Cosine::Builder(BasedInteger::Builder(2)),
-      Rational::Builder(1));
+  e = KDiff("Var"_e, KCos(2_e), 1_e, KDiv(1_e, "Var"_e));
 
   // TODO_PCJ: disabled because Var is a multi-letter variable
   // assert_parsed_layout_is(l, e);
@@ -220,34 +177,12 @@ QUIZ_CASE(poincare_layout_to_expression_parsable) {
   // diff(1/Var, Var, cos(2), 2)
   l = KRackL(
       KNthDiffL("Var"_l, "cos(2)"_l, "2"_l, KRackL(KFracL("1"_l, "Var"_l))));
-  e = Derivative::Builder(
-      Division::Builder(BasedInteger::Builder(1), Symbol::Builder("Var", 3)),
-      Symbol::Builder("Var", 3), Cosine::Builder(BasedInteger::Builder(2)),
-      BasedInteger::Builder(2));
+  e = KDiff("Var"_e, KCos(2_e), 2_e, KDiv(1_e, "Var"_e));
 
   // assert_parsed_layout_is(l, e);
 
   // Piecewise
-  using namespace Poincare::Internal;
-  Tree* p = KRackL(KPiecewise3L("3"_l, "2>3"_l, "2"_l, "2<3"_l, "1"_l, ""_l))
-                ->cloneTree();
-  l = Layout::Builder(p);
-
-  OList args = OList::Builder();
-  args.addChildAtIndexInPlace(BasedInteger::Builder(3), 0, 0);
-  args.addChildAtIndexInPlace(
-      Comparison::Builder(BasedInteger::Builder(2),
-                          ComparisonNode::OperatorType::Superior,
-                          BasedInteger::Builder(3)),
-      1, 1);
-  args.addChildAtIndexInPlace(BasedInteger::Builder(2), 2, 2);
-  args.addChildAtIndexInPlace(
-      Comparison::Builder(BasedInteger::Builder(2),
-                          ComparisonNode::OperatorType::Inferior,
-                          BasedInteger::Builder(3)),
-      3, 3);
-  args.addChildAtIndexInPlace(BasedInteger::Builder(1), 4, 4);
-  e = PiecewiseOperator::UntypedBuilder(args);
-
+  l = KRackL(KPiecewise3L("3"_l, "2>3"_l, "2"_l, "2<3"_l, "1"_l, ""_l));
+  e = KPiecewise(3_e, KSuperior(2_e, 3_e), 2_e, KInferior(2_e, 3_e), 1_e);
   assert_parsed_layout_is(l, e);
 }
