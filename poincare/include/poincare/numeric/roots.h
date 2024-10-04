dfedf6eceab1b81495b0aa5eedb3f7989d79d474
LoreneNumworks <lorene.imperor@numworks.com>
2024-10-04 16:28:08 +0200

    refactor(poincare/roots): remove old code, small adjustments

--- poincare/include/poincare/numeric/roots.h
+++ poincare/include/poincare/numeric/roots.h
@@ -97,51 +97,6 @@ class Roots {
   static Tree* CardanoNumber(const Tree* delta0, const Tree* delta1);
   static Tree* CardanoRoot(const Tree* a, const Tree* b, const Tree* cardano,
                            const Tree* delta0, uint8_t k);
-
-#if 0
-  static int LinearPolynomialRoots(OExpression a, OExpression b,
-                                   OExpression* root,
-                                   ReductionContext reductionContext,
-                                   bool beautifyRoots = true);
-
-  static int QuadraticPolynomialRoots(OExpression a, OExpression b,
-                                      OExpression c, OExpression* root1,
-                                      OExpression* root2, OExpression* delta,
-                                      ReductionContext reductionContext,
-                                      bool* approximateSolutions = nullptr,
-                                      bool beautifyRoots = true);
-
-  static int CubicPolynomialRoots(OExpression a, OExpression b, OExpression c,
-                                  OExpression d, OExpression* root1,
-                                  OExpression* root2, OExpression* root3,
-                                  OExpression* delta,
-                                  ReductionContext reductionContext,
-                                  bool* approximateSolutions = nullptr,
-                                  bool beautifyRoots = true);
-
- private:
-  constexpr static int k_maxNumberOfNodesBeforeApproximatingDelta = 16;
-  static OExpression ReducePolynomial(const OExpression* coefficients,
-                                      int degree, OExpression parameter,
-                                      const ReductionContext& reductionContext);
-  static Rational ReduceRationalPolynomial(const Rational* coefficients,
-                                           int degree, Rational parameter);
-  static bool IsRoot(const OExpression* coefficients, int degree,
-                     OExpression root,
-                     const ReductionContext& reductionContext) {
-    return ReducePolynomial(coefficients, degree, root, reductionContext)
-               .isNull(reductionContext.context()) == OMG::Troolean::True;
-  }
-  static OExpression RationalRootSearch(
-      const OExpression* coefficients, int degree,
-      const ReductionContext& reductionContext);
-  static OExpression SumRootSearch(const OExpression* coefficients, int degree,
-                                   int relevantCoefficient,
-                                   const ReductionContext& reductionContext);
-  static OExpression CardanoNumber(OExpression delta0, OExpression delta1,
-                                   bool* approximate,
-                                   const ReductionContext& reductionContext);
-#endif
 };
 
 }  // namespace Internal
