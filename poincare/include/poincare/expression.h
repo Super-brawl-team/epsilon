1e55ef6e7a437678f0e87618186573cea1718bb7
Pia Chancerel <pia.chancerel@numworks.com>
2024-12-11 13:46:41 +0100

    refactor: remove poincare/include/poincare/expression.h

--- poincare/include/poincare/expression.h
+++
@@ -1,9 +0,0 @@
-#ifndef POINCARE_EXPRESSION_ALIAS_H
-#define POINCARE_EXPRESSION_ALIAS_H
-
-// clang-format off
-// Force the include order
-#include <poincare/old/junior_expression.h>
-// clang-format on
-
-#endif
