4b4b051b386180d1d703350b4bee55c175f47962
Félix Richart <felix.richart@numworks.com>
2024-12-16 10:11:55 +0100

    refactor(poincare/preferences): ensure some preferences are not used inside poincaré by removing the methods from poincarejs

--- poincare/include/poincare/preferences.h
+++ poincare/include/poincare/preferences.h
@@ -101,11 +101,15 @@ class __attribute__((packed)) Preferences final {
       SymbolicComputation replaceSymbols =
           SymbolicComputation::ReplaceDefinedSymbols);
 
-  /* TODO_PCJ: WARNING: The following methods should not be called in Poincare
-   * EditionMode, AngleUnt, ComplexFormat, PrintFloatMode and
-   * NumberOfSignificantDigits should not be stored in Preferences, but the
-   * refactor wasn't tackled yet. None of them is currently called in Poincaré
-   * though. */
+  /* WARNING: The following methods should not be called in Poincare
+   * EditionMode, AngleUnit, ComplexFormat, PrintFloatMode and
+   * NumberOfSignificantDigits should not be stored in Poincaré's Preferences,
+   * and rather be passed to methods signatures.
+   * The refactor wasn't tackled yet but none of them is currently called in
+   * Poincaré. To ensure this, they are forbidden in PoincareJS (which doesn't
+   * need them)
+   * TODO_PCJ: Get rid of them entirely */
+#ifndef TARGET_POINCARE_JS
   CalculationPreferences calculationPreferences() const {
     return m_calculationPreferences;
   }
@@ -138,7 +142,11 @@ class __attribute__((packed)) Preferences final {
     m_calculationPreferences.numberOfSignificantDigits =
         numberOfSignificantDigits;
   }
-  // END OF WARNING
+  uint32_t mathPreferencesCheckSum() const {
+    return (static_cast<uint32_t>(complexFormat()) << 8) +
+           static_cast<uint32_t>(angleUnit());
+  }
+#endif
 
   CombinatoricSymbols combinatoricSymbols() const {
     return m_combinatoricSymbols;
@@ -168,11 +176,6 @@ class __attribute__((packed)) Preferences final {
   bool forceExamModeReload() const { return m_forceExamModeReload; }
   ExamMode examMode() const;
   void setExamMode(ExamMode examMode);
-
-  uint32_t mathPreferencesCheckSum() const {
-    return (static_cast<uint32_t>(complexFormat()) << 8) +
-           static_cast<uint32_t>(angleUnit());
-  }
 
  private:
   constexpr static uint8_t k_version = 0;
