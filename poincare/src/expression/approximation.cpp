a7f3d760ab6cc04f0fccf9a50ad68eeef04a714e
PaulineNumWorks <pauline.garelli@numworks.com>
2025-01-17 10:15:40 +0100

    docs: remove outdated note

--- poincare/src/expression/approximation.cpp
+++ poincare/src/expression/approximation.cpp
@@ -1409,7 +1409,6 @@ bool Private::SkipApproximation(TypeBlock type, TypeBlock parentType,
     case Type::PowMatrix:
     case Type::Pow:
     case Type::PowReal:
-      // Note: After projection, Power's second term should always be integer.
       // Only approximate power's index if the entire tree can be approximated.
       return indexInParent == 1 && !previousChildWasApproximated;
     case Type::ListSlice:
