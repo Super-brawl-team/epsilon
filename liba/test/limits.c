4c8c83e8ed17cef6514012744e69b9e1f7c7ea4c
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2025-02-04 15:33:27 +0100

    test: improve liba_limits tests

--- liba/test/limits.c
+++ liba/test/limits.c
@@ -5,10 +5,15 @@
 
 QUIZ_CASE(liba_limits) {
   quiz_assert(INT_MIN < 0);
-  quiz_assert(UINT_MAX > 0);
+  quiz_assert(INT_MAX > 0);
   int min = INT_MIN;
   int max = INT_MAX;
-  quiz_assert(min - 1 == max);
+  quiz_assert(min == -2147483648);
+  quiz_assert(max == 2147483647);
+  quiz_assert(min + max == -1);
+
+  quiz_assert(UINT_MAX > 0);
   uint32_t umax = UINT_MAX;
+  quiz_assert(umax == 4294967295);
   quiz_assert(umax + 1 == 0);
 }
