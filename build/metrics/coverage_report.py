3cc079c44a4d33b0e1ac75e3c9180c8112835cee
LoreneNumworks <lorene.imperor@numworks.com>
2025-02-13 11:03:08 +0100

    feat(coverage): remove function coverage in script

--- build/metrics/coverage_report.py
+++ build/metrics/coverage_report.py
@@ -10,12 +10,6 @@ class Summary:
     total: int
 
 
-@dataclass
-class FullSummary:
-    lines: Summary
-    functions: Summary
-
-
 # We do not display "UBC" (uncovered base coverage)  and "CBC" (covered base coverage), both of which are always present in
 # the differential report, because they are redundant with the coverage summary (hits / total).
 # A detailed description of the categories can be found here: https://manpages.debian.org/unstable/lcov/genhtml.1.en.html#
@@ -33,7 +27,7 @@ categories_to_display = {
 }
 
 
-class FullDiff:
+class Diff:
     def __init__(self):
         self.line_counts = {
             "UNC": 0,
@@ -47,66 +41,46 @@ class FullDiff:
             "DUB": 0,
             "DCB": 0,
         }
-        self.function_counts = {
-            "UNC": 0,
-            "UIC": 0,
-            "LBC": 0,
-            "GNC": 0,
-            "GIC": 0,
-            "GBC": 0,
-            "EUB": 0,
-            "ECB": 0,
-            "DUB": 0,
-            "DCB": 0,
-        }
 
 
-def format_summary(base_summary: FullSummary, head_summary: FullSummary):
+def format_summary(base_summary: Summary, head_summary: Summary):
     return f"""
-  |                       |                        **BASE**                         |                        **HEAD**                         |
-  |-----------------------|:-------------------------------------------------------:|:-------------------------------------------------------:|
-  |  Line coverage rate   |           {base_summary.lines.percentage} %             |            {head_summary.lines.percentage} %            |
-  |     Hits / Total      | {base_summary.lines.hits} / {base_summary.lines.total}  | {head_summary.lines.hits} / {head_summary.lines.total}  |"""
+  |                       |                     **BASE**                      |                     **HEAD**                      |
+  |-----------------------|:-------------------------------------------------:|:-------------------------------------------------:|
+  |  Line coverage rate   |           {base_summary.percentage} %             |            {head_summary.percentage} %            |
+  |     Hits / Total      |     {base_summary.hits} / {base_summary.total}    |    {head_summary.hits} / {head_summary.total}     |"""
 
 
-def format_diff(diff: FullDiff):
+def format_diff(diff: Diff):
     output = """
-  |                     |   **Lines**                                   | **Functions**                                         |
-  |---------------------|:---------------------------------------------:|:-----------------------------------------------------:|"""
-
+  |                     |   **Lines**                                   |
+  |---------------------|:---------------------------------------------:|"""
     for category, description in categories_to_display.items():
-        if diff.line_counts[category] > 0 or diff.function_counts[category] > 0:
+        if diff.line_counts[category] > 0:
             output += f"""
-  |     {description}   |        {diff.line_counts[category]}           |             {diff.function_counts[category]}          |"""
+  |     {description}   |        {diff.line_counts[category]}           |"""
     return output
 
 
 def parse_summary(output_txt: str):
     line_summary = None
-    function_summary = None
     with open(output_txt, "r") as f:
         lines = f.readlines()
     for line in lines:
         if line.lstrip().startswith("lines"):
             percentage, hits, total = parse.search(": {}% ({} of {} lines)", line)
             line_summary = Summary(percentage, hits, total)
-        if line.lstrip().startswith("functions"):
-            percentage, hits, total = parse.search(": {}% ({} of {} functions)", line)
-            function_summary = Summary(percentage, hits, total)
-    return FullSummary(line_summary, function_summary)
+    return line_summary
 
 
 def parse_diff(genhtml_txt: str):
     with open(genhtml_txt, "r") as f:
         lines = f.readlines()
-        diff = FullDiff()
+        diff = Diff()
         editing = None
         for line in lines:
             if line.lstrip().startswith("lines"):
                 editing = diff.line_counts
-                continue
-            if line.lstrip().startswith("functions"):
-                editing = diff.function_counts
                 continue
             if line.lstrip().startswith("Message"):
                 break
