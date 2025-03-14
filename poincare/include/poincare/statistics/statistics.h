4827dc2d3dc08a3ad14ab3ae0fb59881899f9b49
Félix Richart <felix.richart@numworks.com>
2025-03-14 16:34:21 +0100

    feat(poincare/poincarejs): bind all dataTable classes

--- poincare/include/poincare/statistics/statistics.h
+++ poincare/include/poincare/statistics/statistics.h
@@ -2,25 +2,23 @@
 #define POINCARE_STATISTICS_STATISTICS_DATASET_H
 
 #include <poincare/src/statistics/data_table.h>
+#include <poincare/src/statistics/dataset_adapter.h>
 #include <poincare/src/statistics/statistics_dataset.h>
 #include <poincare/src/statistics/statistics_dataset_column.h>
 
 namespace Poincare {
 
-template <typename T>
-using DatasetColumn = Internal::DatasetColumn<T>;
+using Internal::ConstantDatasetColumn;
+using Internal::DatasetColumn;
 
-template <typename T>
-using ConstantDatasetColumn = Internal::ConstantDatasetColumn<T>;
+using Internal::StatisticsCalculationOptions;
 
-using StatisticsCalculationOptions = Internal::StatisticsCalculationOptions;
+using Internal::DataTable;
+using Internal::Series;
 
 // Cf internal documentation
-template <typename T>
-using StatisticsDataset = Internal::StatisticsDataset<T>;
-
-using DataTable = Internal::DataTable;
-using Series = Internal::Series;
+using Internal::StatisticsDataset;
+using Internal::StatisticsDatasetFromTable;
 
 }  // namespace Poincare
 #endif
