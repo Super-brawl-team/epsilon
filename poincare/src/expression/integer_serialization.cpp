f123751816fdb07fd55721f6b00239fb16121549
Hugo Saint-Vignes <hugo.saint-vignes@numworks.com>
2025-04-30 14:32:36 +0200

    refactor: optimize integer serialization
    Using the workingBuffer, and factorizing some logic.

--- poincare/src/expression/integer_serialization.cpp
+++ poincare/src/expression/integer_serialization.cpp
@@ -28,6 +28,7 @@ void IntegerHandler::removeZeroAtTheEnd(int minimalNumbersOfDigits,
   if (isZero()) {
     return;
   }
+  uint8_t* const localStart = workingBuffer->localStart();
 
   /* If we check the number of digits, we want *i to stay outside of the
    * interval ]-10^numberDigits; 10^numberDigits[. */
@@ -37,30 +38,30 @@ void IntegerHandler::removeZeroAtTheEnd(int minimalNumbersOfDigits,
   // IntegerHandler builder with int64_t is not implemented yet.
   assert(minimumValue <= UINT32_MAX);
   uint32_t minimumValue32 = static_cast<uint32_t>(minimumValue);
-
+  IntegerHandler base = IntegerHandler(10);
   IntegerHandler minimum =
       // !shouldCheckMinimalNumberOfDigits ? Integer::Overflow(false) :
       IntegerHandler(minimumValue32, NonStrictSign::Positive);
   IntegerHandler minusMinimum =
       // !shouldCheckMinimalNumberOfDigits ? Integer::Overflow(false) :
       IntegerHandler(minimumValue32, NonStrictSign::Negative);
-
-  IntegerHandler base = IntegerHandler(10);
-  DivisionResult<IntegerHandler> d = Udiv(*this, base, workingBuffer);
-  while (d.remainder.isZero()) {
-    if (shouldCheckMinimalNumberOfDigits &&
-        (Compare(d.quotient, minimum) <= 0 &&
-         Compare(d.quotient, minusMinimum) >= 0)) {
-      break;
+  while (true) {
+    DivisionResult<IntegerHandler> d = Udiv(*this, base, workingBuffer);
+    if (!d.remainder.isZero() || (shouldCheckMinimalNumberOfDigits &&
+                                  (Compare(d.quotient, minimum) <= 0 &&
+                                   Compare(d.quotient, minusMinimum) >= 0))) {
+      // assert(!isOverflow());
+      return;
     }
     *this = d.quotient;
-    d = Udiv(*this, base, workingBuffer);
+    workingBuffer->garbageCollect({&base, &minimum, &minusMinimum, this},
+                                  localStart);
   }
-  // assert(!isOverflow());
 }
 
 size_t IntegerHandler::serialize(char* buffer, size_t bufferSize,
                                  WorkingBuffer* workingBuffer) const {
+  uint8_t* const localStart = workingBuffer->localStart();
   if (bufferSize == 0) {
     return bufferSize - 1;
   }
@@ -69,17 +70,23 @@ size_t IntegerHandler::serialize(char* buffer, size_t bufferSize,
     return bufferSize - 1;
   }
 
-  IntegerHandler base(10);
-  DivisionResult<IntegerHandler> d = Udiv(*this, base, workingBuffer);
-
   size_t length = 0;
   if (isZero()) {
     length += WriteCodePoint(buffer + length, bufferSize - length, '0');
+    assert(length <= bufferSize - 1);
+    return length;
   } else if (m_sign == NonStrictSign::Negative) {
     length += WriteCodePoint(buffer + length, bufferSize - length, '-');
   }
 
-  while (!(d.remainder.isZero() && d.quotient.isZero())) {
+  IntegerHandler base(10);
+  IntegerHandler quotient = *this;
+  while (true) {
+    DivisionResult<IntegerHandler> d = Udiv(quotient, base, workingBuffer);
+    if (d.remainder.isZero() && d.quotient.isZero()) {
+      break;
+    }
+    quotient = d.quotient;
     char c = OMG::Print::CharacterForDigit(
         OMG::Base::Decimal, d.remainder.isZero() ? 0 : d.remainder.digit(0));
     if (length >= bufferSize - 1) {
@@ -89,8 +96,8 @@ size_t IntegerHandler::serialize(char* buffer, size_t bufferSize,
                  Preferences::PrintFloatMode::Decimal)
           .CharLength;
     }
+    workingBuffer->garbageCollect({&base, &quotient}, localStart);
     length += WriteCodePoint(buffer + length, bufferSize - length, c);
-    d = Udiv(d.quotient, base, workingBuffer);
   }
   assert(length <= bufferSize - 1);
   assert(buffer[length] == 0);
