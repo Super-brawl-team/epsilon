f8cebb7542f734bd831c816f1930d0d1954b2095
LoreneNumworks <lorene.imperor@numworks.com>
2025-05-12 15:39:35 +0200

    feat(expression): create the SerializedExpression header

---
+++ poincare/include/poincare/serialized_expression.h
@@ -0,0 +1,39 @@
+#ifndef POINCARE_SERIALIZED_EXPRESSION_H
+#define POINCARE_SERIALIZED_EXPRESSION_H
+
+#include "expression.h"
+
+namespace Poincare {
+
+template <size_t MaxLength>
+class SerializedExpression {
+ public:
+  SerializedExpression() = default;
+
+  explicit SerializedExpression(Expression expression) {
+    [[maybe_unused]] size_t usedLength = expression.serialize(
+        m_buffer, MaxLength, Preferences::PrintFloatMode::Decimal,
+        PrintFloat::k_maxNumberOfSignificantDigits);
+    assert(usedLength <= MaxLength);
+  }
+
+  bool isUninitialized() const { return m_buffer[0] == '\0'; }
+
+  const char* text() const { return m_buffer; }
+
+  Expression expression() const {
+    assert(!isUninitialized());
+    return Expression::Parse(m_buffer, nullptr);
+  }
+
+  bool operator==(const SerializedExpression& other) const {
+    return strcmp(m_buffer, other.m_buffer) == 0;
+  }
+
+ private:
+  char m_buffer[MaxLength] = "";
+};
+
+}  // namespace Poincare
+
+#endif
