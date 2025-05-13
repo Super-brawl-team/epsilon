13314373a621228be02ab1d0f1b6ce738fefe83d
MarcNumworks <marc.coudriau@numworks.com>
2025-05-13 09:44:04 +0200

    fix(ion/simulator): change dummy battery voltage

--- ion/src/shared/dummy/battery.cpp
+++ ion/src/shared/dummy/battery.cpp
@@ -4,4 +4,4 @@ bool Ion::Battery::isCharging() { return false; }
 
 Ion::Battery::Charge Ion::Battery::level() { return Charge::FULL; }
 
-float Ion::Battery::voltage() { return 0.0f; }
+float Ion::Battery::voltage() { return 4.2f; }
