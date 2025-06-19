196dd535ceea7625841909e970d389ed1a30597f
LoreneNumworks <lorene.imperor@numworks.com>
2025-06-19 16:55:18 +0200

    refactor(poincare): assert out of range decoder

--- poincare/src/layout/layout_span_decoder.cpp
+++ poincare/src/layout/layout_span_decoder.cpp
@@ -1,9 +1,20 @@
 #include "layout_span_decoder.h"
 
+#include <stdint.h>
+
 namespace Poincare::Internal {
 
 void LayoutSpanDecoder::next() {
+#if ASSERTIONS
   // next will be called when m_length is 0 but normally only once
+  constexpr uint16_t k_outOfRange = UINT16_MAX;
+  assert(m_length != k_outOfRange);
+  if (m_length == 0) {
+    m_length = k_outOfRange;
+    return;
+  }
+#endif
+
   if (m_length > 0) {
     assert(m_layout != nullptr);
     m_layout = static_cast<const Layout*>(m_layout->nextTree());
